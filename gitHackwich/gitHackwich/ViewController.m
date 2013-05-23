6f9a78d32ea008d7c088d6b3448fe9a4b8e704a4
Erin Hochstatter <ehochstatter@gmail.com>
2013-05-23 11:15:42 -0500

    second commit redux

--- gitHackwich/gitHackwich/ViewController.m
+++ gitHackwich/gitHackwich/ViewController.m
@@ -18,6 +18,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"git up-ah, git on up");
+    NSLog(@"second try to get an M by my file");
+   
 }
 
 - (void)didReceiveMemoryWarning
